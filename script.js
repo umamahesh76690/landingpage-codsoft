6675a8baa0a9343a0e6a9742c88f1998a666d763
umamahesh76690 <113962453+umamahesh76690@users.noreply.github.com>
2024-06-22 12:31:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+const products = [];
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    choosenProduct = products[index];
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
